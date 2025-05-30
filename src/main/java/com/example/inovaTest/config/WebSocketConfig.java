939dc0ff6313fba6a8d983c00a5380aa3b71c3cf
Everton Farias <evertondefarias115@gmail.com>
2025-05-29 23:57:43 -0300

    ok

--- src/main/java/com/example/inovaTest/config/WebSocketConfig.java
+++ src/main/java/com/example/inovaTest/config/WebSocketConfig.java
@@ -17,7 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:*")
+                .setAllowedOriginPatterns("http://31.97.130.19:*")
                 .addInterceptors(authHandshakeInterceptor)
                 .withSockJS();
     }
